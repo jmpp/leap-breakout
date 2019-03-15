705e1981b1a4aafdb0d33e01a82cfc7d53e60237
J.M. Cléry <jmclery@gmail.com>
2019-03-15 11:01:13 +0100

    Cloisonnement en modules ES5

--- scripts/index.js
+++ scripts/index.js
@@ -1,112 +1,116 @@
-const game = new Phaser.Game(
-    window.innerWidth,
-    window.innerHeight,
-    Phaser.AUTO,
-    'game-root',
-    {
-        preload: preload,
-        create: create,
-        update: update,
-        render: render
+(function(APP) {
+
+    const game = new Phaser.Game(
+        window.innerWidth,
+        window.innerHeight,
+        Phaser.AUTO,
+        'game-root',
+        {
+            preload: preload,
+            create: create,
+            update: update,
+            render: render
+        }
+    );
+    
+    let ball;
+    let pad;
+    let bricksGroup;
+    let timerText, timer = 0;
+    
+    function preload() {
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        // game.scale.pageAlignHorizontally = true;
+        // game.scale.pageAlignVertically = true;
+        // game.stage.backgroundColor = '#eee';
+    
+        game.load.image('ball', '../images/ball.png');
+        game.load.image('pad', '../images/pad.png');
+        game.load.image('brick_1', '../images/01.png', 100, 33);
+        game.load.image('brick_2', '../images/02.png', 100, 33);
+        game.load.image('brick_3', '../images/03.png', 100, 33);
+        game.load.image('brick_4', '../images/04.png', 100, 33);
+        game.load.image('brick_5', '../images/05.png', 100, 33);
+        game.load.image('brick_6', '../images/06.png', 100, 33);
+        game.load.image('brick_7', '../images/07.png', 100, 33);
+        game.load.image('brick_8', '../images/08.png', 100, 33);
+        game.load.image('brick_9', '../images/09.png', 100, 33);
+        game.load.image('brick_10', '../images/10.png', 100, 33);
     }
-);
-
-let ball;
-let pad;
-let bricksGroup;
-let timerText, timer = 0;
-
-function preload() {
-    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-    // game.scale.pageAlignHorizontally = true;
-    // game.scale.pageAlignVertically = true;
-    // game.stage.backgroundColor = '#eee';
-
-    game.load.image('ball', '../images/ball.png');
-    game.load.image('pad', '../images/pad.png');
-    game.load.image('brick_1', '../images/01.png', 100, 33);
-    game.load.image('brick_2', '../images/02.png', 100, 33);
-    game.load.image('brick_3', '../images/03.png', 100, 33);
-    game.load.image('brick_4', '../images/04.png', 100, 33);
-    game.load.image('brick_5', '../images/05.png', 100, 33);
-    game.load.image('brick_6', '../images/06.png', 100, 33);
-    game.load.image('brick_7', '../images/07.png', 100, 33);
-    game.load.image('brick_8', '../images/08.png', 100, 33);
-    game.load.image('brick_9', '../images/09.png', 100, 33);
-    game.load.image('brick_10', '../images/10.png', 100, 33);
-}
-
-function create() {
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-
-    // Création du groupe de briques
-    bricksGroup = game.add.physicsGroup();
-    // Génération des briques
-    for (let y = 0; y < 5; y++) {
-        for (let x = 0; x < 10; x++) {
-            let sprite = new Phaser.Sprite(game, x * 100 + (game.world.width - 1000) / 2, y * 33, 'brick_' + (x % 10 + 1));
-            // sprite.scale.setTo(0.2604, 0.2604); 
-            bricksGroup.add(sprite);
+    
+    function create() {
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+    
+        // Création du groupe de briques
+        bricksGroup = game.add.physicsGroup();
+        // Génération des briques
+        for (let y = 0; y < 5; y++) {
+            for (let x = 0; x < 10; x++) {
+                let sprite = new Phaser.Sprite(game, x * 100 + (game.world.width - 1000) / 2, y * 33, 'brick_' + (x % 10 + 1));
+                // sprite.scale.setTo(0.2604, 0.2604); 
+                bricksGroup.add(sprite);
+            }
+        }
+        bricksGroup.forEach(item => {
+            item.body.immovable = true;
+        });
+    
+        // Création de la balle
+        ball = game.add.sprite(game.world.width / 2, game.world.height - 100, 'ball');
+        ball.anchor.set(0.5);
+        game.physics.arcade.enable(ball);
+        ball.body.collideWorldBounds = true;
+        ball.body.velocity.set(300, -300);
+        ball.body.bounce.set(1);
+        
+        // Création du pad
+        pad = game.add.sprite(game.world.width / 2, game.world.height - 5, 'pad');
+        pad.anchor.set(0.5, 1);
+        game.physics.arcade.enable(pad);
+        pad.body.immovable = true;
+    
+        // La balle peut sortir en bas de l'écran
+        game.physics.arcade.checkCollision.down = false;
+        ball.checkWorldBounds = true;
+        ball.events.onOutOfBounds.add(function(){
+            // window.alert('Game over!');
+            // window.location.reload();
+        }, this);
+    
+        // Timing
+        timerText = game.add.text(5, 5, 'Temps: 0.0s', { font: '18px Arial', fill: '#ffff00' });
+        setInterval(() => timer += 100, 100);
+    }
+    
+    function update() {
+        game.physics.arcade.collide(ball, bricksGroup, ballHitBrick);
+        game.physics.arcade.collide(ball, pad, ballHitPad);
+    
+        if (APP.LeapInput.connected) {
+            pad.x = APP.LeapInput.position.x;
+        } else {
+            pad.x = game.input.x || game.world.width * 0.5;
+        }
+    
+        timerText.setText('Temps: ' + (timer / 1000).toFixed(1) + 's');
+    }
+    
+    function render() {
+    
+    }
+    
+    function ballHitBrick(ball, brick) {
+        brick.kill();
+    
+        const remainingBalls = bricksGroup.children.reduce((total, brick) => brick.alive ? total + 1 : total, 0);
+        if (remainingBalls === 0) {
+            window.alert('Bravo!\nVous avez complété le jeu en '+ (timer / 1000).toFixed(1) +' secondes !');
+            window.location.reload();
         }
     }
-    bricksGroup.forEach(item => {
-        item.body.immovable = true;
-    });
-
-    // Création de la balle
-    ball = game.add.sprite(game.world.width / 2, game.world.height - 100, 'ball');
-    ball.anchor.set(0.5);
-    game.physics.arcade.enable(ball);
-    ball.body.collideWorldBounds = true;
-    ball.body.velocity.set(300, -300);
-    ball.body.bounce.set(1);
     
-    // Création du pad
-    pad = game.add.sprite(game.world.width / 2, game.world.height - 5, 'pad');
-    pad.anchor.set(0.5, 1);
-    game.physics.arcade.enable(pad);
-    pad.body.immovable = true;
-
-    // La balle peut sortir en bas de l'écran
-    game.physics.arcade.checkCollision.down = false;
-    ball.checkWorldBounds = true;
-    ball.events.onOutOfBounds.add(function(){
-        // window.alert('Game over!');
-        // window.location.reload();
-    }, this);
-
-    // Timing
-    timerText = game.add.text(5, 5, 'Temps: 0.0s', { font: '18px Arial', fill: '#ffff00' });
-    setInterval(() => timer += 100, 100);
-}
-
-function update() {
-    game.physics.arcade.collide(ball, bricksGroup, ballHitBrick);
-    game.physics.arcade.collide(ball, pad, ballHitPad);
-
-    if (LeapInput.connected) {
-        pad.x = LeapInput.position.x;
-    } else {
-        pad.x = game.input.x || game.world.width * 0.5;
+    function ballHitPad(ball, pad) {
+        ball.body.velocity.x = -1 * 5 * (pad.x - ball.x);
     }
-
-    timerText.setText('Temps: ' + (timer / 1000).toFixed(1) + 's');
-}
-
-function render() {
-
-}
-
-function ballHitBrick(ball, brick) {
-    brick.kill();
-
-    const remainingBalls = bricksGroup.children.reduce((total, brick) => brick.alive ? total + 1 : total, 0);
-    if (remainingBalls === 0) {
-        window.alert('Bravo!\nVous avez complété le jeu en '+ (timer / 1000).toFixed(1) +' secondes !');
-        window.location.reload();
-    }
-}
-
-function ballHitPad(ball, pad) {
-    ball.body.velocity.x = -1 * 5 * (pad.x - ball.x);
-}
+        
+})(window.APP);
--- scripts/leapInput.js
+++ scripts/leapInput.js
@@ -1,47 +1,54 @@
-const LeapInput = {
-    position  : { x : 0, y : 0 },
-    connected : false,
-};
+(function(APP) {
 
-const controller = new Leap.Controller();
-
-controller.on('deviceStreaming', onDeviceStreaming);
-controller.on('deviceDisconnected', onDeviceDisconnected);
-controller.on('frame', onFrame);
-
-controller.connect();
-
-
-function onDeviceStreaming() {
-    LeapInput.connected = true;
-    console.log('✅ Leap Motion is streaming!');
-}
-
-function onDeviceDisconnected() {
-    LeapInput.connected = false;
-    console.log('❌ Leap Motion is disconnected!');
-}
-
-function onFrame(frame) {
-    let hand = frame.hands[0];
-    if (!hand) return;
-
-    const palm = get2dCoords(hand.stabilizedPalmPosition, frame);
-    LeapInput.position.x = palm.x;
-    LeapInput.position.y = palm.y;
-}
-
-/**
- * Transforme les coordonnées 3D récupérée par le Leap en coordonnées 2D pour un <canvas> web
- * @param {Array} leapPosition Tableau de coordonnées 3D [x, y, z]
- * @param {Object} frame Objet "frame" transmit par le Leap Motion
- */
-function get2dCoords(leapPosition, frame) {
-    const interactionBox  = frame.interactionBox;
-    const normalizedPoint = interactionBox.normalizePoint(leapPosition, true);
-
-    return {
-        x : normalizedPoint[0] * window.innerWidth,
-        y : (1 - normalizedPoint[1]) * window.innerHeight,
+    const LeapInput = {
+        position  : { x : 0, y : 0 },
+        connected : false,
+    };
+    
+    const controller = new Leap.Controller();
+    
+    controller.on('deviceStreaming', onDeviceStreaming);
+    controller.on('deviceDisconnected', onDeviceDisconnected);
+    controller.on('frame', onFrame);
+    
+    controller.connect();
+    
+    
+    function onDeviceStreaming() {
+        LeapInput.connected = true;
+        console.log('✅ Leap Motion is streaming!');
     }
-}
+    
+    function onDeviceDisconnected() {
+        LeapInput.connected = false;
+        console.log('❌ Leap Motion is disconnected!');
+    }
+    
+    function onFrame(frame) {
+        let hand = frame.hands[0];
+        if (!hand) return;
+    
+        const palm = get2dCoords(hand.stabilizedPalmPosition, frame);
+        LeapInput.position.x = palm.x;
+        LeapInput.position.y = palm.y;
+    }
+    
+    /**
+     * Transforme les coordonnées 3D récupérée par le Leap en coordonnées 2D pour un <canvas> web
+     * @param {Array} leapPosition Tableau de coordonnées 3D [x, y, z]
+     * @param {Object} frame Objet "frame" transmit par le Leap Motion
+     */
+    function get2dCoords(leapPosition, frame) {
+        const interactionBox  = frame.interactionBox;
+        const normalizedPoint = interactionBox.normalizePoint(leapPosition, true);
+    
+        return {
+            x : normalizedPoint[0] * window.innerWidth,
+            y : (1 - normalizedPoint[1]) * window.innerHeight,
+        }
+    }
+
+    // Module export
+    APP.LeapInput = LeapInput;
+
+})(window.APP);
